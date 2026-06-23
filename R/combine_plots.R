34fa10fe6ccfc14c4b237d0c92cadd32875146b5
Isabell Victoria Strandby Ernst <Isabell.ernst@hotmail.com>
2026-06-23 15:25:10 +0200

    combine all plots, with my theme

--- R/combine_plots.R
+++ R/combine_plots.R
@@ -13,15 +13,33 @@ gtt_perc
 gtt_auc_perc
 itt_perc
 itt_auc_perc
+gtt_raw
+itt_raw
 
 # Load the library
 library(ggplot2)
 library(patchwork)
 
-# Arrange plots with patchwork
-layout <- (weight | glucose | insulin | spina_gr | spina_gbeta | gtt_perc | gtt_auc_perc | itt_perc | itt_auc_perc)
+my_theme <- function() {
+    ggplot2::theme_classic(base_size = 7) +
+        ggplot2::theme(
+            panel.grid.major = ggplot2::element_line(color = "grey85"),
+            panel.grid.minor = ggplot2::element_line(color = "grey95")
+        )
+}
+
+layout <- (plot_spacer() | weight | glucose | insulin | spina_gr | spina_gbeta | gtt_perc | gtt_auc_perc | itt_perc | itt_auc_perc) & my_theme() +
+    ggplot2::theme(legend.position = "none", strip.text = ggplot2::element_blank())
 pdf(here::here("data/combined_plots.pdf"),
-    width = 25,
-    height = 5)
-layout + plot_layout(widths = c(0.75, 0.75, 0.75, 0.75, 0.75, 2, 0.75, 2, 0.75))
+    width = 12,
+    height = 2)
+layout  + plot_layout(widths = c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 0.5, 1, 0.5))
 dev.off()
+
+pdf(here::here("data/combined_plots_raw.pdf"),
+    width = 4,
+    height = 2)
+(gtt_raw | itt_raw) & my_theme() +
+    ggplot2::theme(legend.position = "none", strip.text = ggplot2::element_blank())
+dev.off()
+
